1c5aa084b21e055fdd33506cb044612951976bd7
Psivaranjani <sathyapalanichamy90@gmail.com>
2017-07-10 20:56:28 +0530

    Create DelMin.java

---
+++ DelMin.java
@@ -0,0 +1,16 @@
+# pro
+import java.util.*;
+public class DelMin
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		int k=sc.nextInt();
+		String s[]=str.split("");
+		Arrays.sort(s);
+		str=Arrays.toString(s);
+		str=str.replaceAll("\\[","").replaceAll("\\]","").replaceAll("\\s","").replaceAll(",","");
+		System.out.println(str.substring(0, s.length-k));		
+	}
+}
